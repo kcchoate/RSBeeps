80cc3558f1150ff13f4aa5b518e697a1b6a23f69
Kendrick Choate <kendrickchoate@gmail.com>
2017-11-29 19:04:14 -0600

    initial commit; no error handling but it works for now. Simple count down that beeps every 50 seconds.

--- RSBeeps/Form1.Designer.cs
+++ RSBeeps/Form1.Designer.cs
@@ -29,11 +29,45 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(120, 115);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "49";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(145, 139);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RS Beeps";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- RSBeeps/Form1.cs
+++ RSBeeps/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Media;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,34 @@ namespace RSBeeps
 {
     public partial class Form1 : Form
     {
+        private static int remainingTime = 49;
+
         public Form1()
         {
             InitializeComponent();
+
+            //set the timer to tick every 1 second
+            timer1.Interval = 1000;
+
+            //start the timer. The timer will call the timer1_Tick event handler function every 1 second
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SoundPlayer beep = new SoundPlayer("..\\..\\beep.wav");
+            if (remainingTime == 49)
+            {
+                //drop the beep
+                beep.Play();
+            }
+            //bring the beep back and shout it loud
+            richTextBox1.Text = (--remainingTime).ToString();
+
+            if (remainingTime == 0)
+            {
+                remainingTime = 49;
+            }
         }
     }
 }
