2d3d9e4c72ede4b41f16551a62749f265ed31d82
Kendrick Choate <kendrickchoate@gmail.com>
2017-11-29 19:16:56 -0600

    Modified program to beep on initialization since the timer.Start() function waits the Interval time before firing for the first time. tick function now makes sure we are not on the first beep's cycle before beeping to prevent two beeps in two seconds in the first run.

--- RSBeeps/Form1.cs
+++ RSBeeps/Form1.cs
@@ -13,23 +13,28 @@ namespace RSBeeps
 {
     public partial class Form1 : Form
     {
+        //set to 48 so the initial beep is not followed by a beep from the timer
         private static int remainingTime = 49;
+        private static bool firstbeep = true;
+        static SoundPlayer beep = new SoundPlayer("C:\\Program Files (x86)\\RSBeeps\\Media\\beep.wav");
 
         public Form1()
         {
             InitializeComponent();
 
+            //play an initial beep of the timer so that it beeps when started (otherwise it waits one second)
+            beep.Play();
+
             //set the timer to tick every 1 second
             timer1.Interval = 1000;
-
+            
             //start the timer. The timer will call the timer1_Tick event handler function every 1 second
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            SoundPlayer beep = new SoundPlayer("..\\..\\beep.wav");
-            if (remainingTime == 49)
+            if (remainingTime == 49 && !firstbeep)
             {
                 //drop the beep
                 beep.Play();
@@ -41,6 +46,9 @@ namespace RSBeeps
             {
                 remainingTime = 49;
             }
+
+            //set first beep to false
+            firstbeep = false;
         }
     }
 }
